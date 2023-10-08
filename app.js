e6cb5793c0ef2451ea413e8b9571950201924155
AndresCabarcas <andacames@gmail.com>
2023-10-08 17:49:35 -0500

    mail-api

--- app.js
+++ app.js
@@ -1,13 +1,43 @@
-const express = require('express');
+const express = require("express");
+const cors = require("cors");
+const dotenv = require('dotenv');
+const { Resend } = require('resend');
+
 const app = express();
-const port = process.env.PORT ?? 3000;
+const port = 3000;
 
-app.use(express.static('public'))
+dotenv.config();
+app.use(cors({ origin: "*" }));
+app.use(express.json()); 
 
-app.get('*', (req, res) => {
-    res.redirect('/');
-})
+// Create a new Resend instance
+const resend = new Resend('re_UnFiSRZT_BPEFMnGhBBDNQTJ4ZLy2Nn95');
+
+app.get("/", (req, res) => {
+  res.send("Why so serious?");
+});
+
+// html function 
+const html = require('./HTMLCreator.js'); 
+app.post("/api/email", async (req, res) => {
+    const { sAsunto, sEmail, sNombre, sMessage } = req.body;
+    
+    try {
+      const data = await resend.emails.send({
+        from: 'pqrs@asorecicol.com',
+        to: 'pqrs.asorecicol@gmail.com',
+        subject: sAsunto,
+        html: html(sAsunto, sEmail, sNombre, sMessage)
+      });
+
+      console.log(data);
+      res.json({ message: 'Mensaje enviado exitosamente...' });
+    } catch (error) {
+      console.error(error);
+      res.json(error);
+    }
+});
 
 app.listen(port, () => {
-    console.log(`App listening on port ${port}`);
-})
+  console.log(`Example app listening on port ${port}`);
+});
